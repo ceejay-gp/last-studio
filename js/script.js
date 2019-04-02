28f2d20ffd8ab8a4f14bc3b9359da63959122c2c
Chris <rumdrunkmonkey@hotmail.co.uk>
2019-04-02 16:18:20 +0100

    Javascript
    Started adding javascript

--- js/script.js
+++ js/script.js
@@ -1,48 +1,18 @@
 /*jshint esversion: 6 */
 
-// Lightbox functionality
-// the first step we want to do is to show our lightbox when we click on an image
-// we want to make sure that the image we clicked on will be in the lightbox
-// we want to make sure that when we click on the lightbox when its visible that it will fade out and hide itself again
+// We need to listen for scroll events on the page
+// When the scrollbar reaches a certain distance
+// We want to change the background color of the body
 
-$('.brogues img, .stationary img').on('click', function() {
-  $('.lightbox').fadeIn();
+const bodyTag = document.querySelector("body");
+const h1Tag = document.querySelector("h1");
 
-  let imageSrc = $(this).attr('src');
-  $('.lightbox img').attr('src', imageSrc);
+h1Tag.addEventListener("click", function() {
+  // something happens when h1 is clicked
+  // bodyTag.innerHTML = "h1 has been clicked!";
+  bodyTag.style.fontSize = "50px";
+  bodyTag.style.backgroundColor = "green";
 });
 
-$('.lightbox').on('click', function() {
-  $('.lightbox').fadeOut();
-});
-
-// We first need to listen for the scroll event on the page
-// Next, when our browser reaches a certain distance form the top of the page we need to change the body elements background color
-
-const body = document.querySelector("body");
-
-// Listen out for scroll events on our page
-document.addEventListener("scroll", function() {
-  //console.log("Page scrolled");
-
-  // Find out and store how far from the top of the page the scrollbar is
-  let distanceFromTop = window.scrollY;
-  console.log(distanceFromTop);
-
-  // If distanceFromTop is greater than 1000 pixels, do something
-  if (distanceFromTop > 1000) {
-    console.log("past 1000 pixels");
-    body.classList.add("brogue-active");
-  } else {
-    // Otherwise do something else
-    console.log("less than 1000 pixels down");
-    body.classList.remove("brogue-active");
-  }
-
-  if (distanceFromTop > 4400) {
-    body.classList.add("stationary-active");
-  } else {
-    body.classList.remove("stationary-active");
-  }
-
-});
+// bodyTag.style.backgroundColor = "green";
+// bodyTag.innerHTML = "Hi I'm Javascript! Your site is now ruined! hehe!";
